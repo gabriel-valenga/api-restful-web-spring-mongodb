dd024f4160f2f61c6950b00468d475e5e1424619
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2019-03-07 20:47:40 -0300

    Post comments

--- src/main/java/com/cursonelioalves/javaspringweb/config/Instantiation.java
+++ src/main/java/com/cursonelioalves/javaspringweb/config/Instantiation.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import com.cursonelioalves.javaspringweb.domain.Post;
 import com.cursonelioalves.javaspringweb.domain.User;
 import com.cursonelioalves.javaspringweb.dto.AuthorDTO;
+import com.cursonelioalves.javaspringweb.dto.CommentDTO;
 import com.cursonelioalves.javaspringweb.repository.PostRepository;
 import com.cursonelioalves.javaspringweb.repository.UserRepository;
 
@@ -44,6 +45,14 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu vagem!", "Vou viajar para São Paulo", new AuthorDTO(bob));
 		Post post2 = new Post(null, sdf.parse("08/06/2018"), "Bom dia!", "Bom dia a todos!", new AuthorDTO(alex));
 				
+		CommentDTO c1 = new CommentDTO("Boa Viagem!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Traga presentes!", sdf.parse("21/03/2018"), new AuthorDTO(maria));
+		CommentDTO c3 = new CommentDTO("Bom dia!", sdf.parse("08/06/2018"), new AuthorDTO(bob));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		alex.getPosts().addAll(Arrays.asList(post2));
