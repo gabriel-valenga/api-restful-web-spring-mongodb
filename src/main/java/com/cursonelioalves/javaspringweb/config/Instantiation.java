638a0f8848c6292261cd665770d3941595809919
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2019-03-07 22:03:06 -0300

    Query methods

--- src/main/java/com/cursonelioalves/javaspringweb/config/Instantiation.java
+++ src/main/java/com/cursonelioalves/javaspringweb/config/Instantiation.java
@@ -42,7 +42,7 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu vagem!", "Vou viajar para São Paulo", new AuthorDTO(bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo", new AuthorDTO(bob));
 		Post post2 = new Post(null, sdf.parse("08/06/2018"), "Bom dia!", "Bom dia a todos!", new AuthorDTO(alex));
 				
 		CommentDTO c1 = new CommentDTO("Boa Viagem!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
